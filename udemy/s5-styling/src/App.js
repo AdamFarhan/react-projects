4bc0c84666b510f93884b00e1f2b230598899bd5
Adam <adamfarhan11@gmail.com>
2019-05-29 19:00:21 -0400

    Added hover effects with Radium

--- udemy/s5-styling/src/App.js
+++ udemy/s5-styling/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -64,7 +65,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black',
+      }
     };
     let people = null;
 
@@ -90,6 +95,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black',
+      }
     }
 
     //returns 1 string ("red bold")
@@ -115,4 +124,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
