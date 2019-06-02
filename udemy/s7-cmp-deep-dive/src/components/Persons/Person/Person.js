2a92ca1f6d92ea6914c34d9e7b3a3071a55bd0db
Adam <adamfarhan11@gmail.com>
2019-06-02 17:03:27 -0400

    Added element refs

--- udemy/s7-cmp-deep-dive/src/components/Persons/Person/Person.js
+++ udemy/s7-cmp-deep-dive/src/components/Persons/Person/Person.js
@@ -4,6 +4,15 @@ import withClass from '../../../hoc/withClass';
 import PropTypes from 'prop-types';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+  componentDidMount() {
+    console.log(this.inputElementRef.current);
+    this.inputElementRef.current.focus();
+    //this.inputElement.focus();
+  }
   render() {
     console.log('[Person.js] rendering...');
     return (
@@ -16,6 +25,8 @@ class Person extends Component {
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
+          //ref={(inputEl) => {this.inputElement = inputEl}}
+          ref={this.inputElementRef}
         />
       </React.Fragment>
     );
