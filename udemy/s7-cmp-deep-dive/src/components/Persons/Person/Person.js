ae756914c3d552be9a85d78ade89f978f711bbfa
Adam <adamfarhan11@gmail.com>
2019-06-02 17:48:56 -0400

    Used context to pass data between components

--- udemy/s7-cmp-deep-dive/src/components/Persons/Person/Person.js
+++ udemy/s7-cmp-deep-dive/src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
 import PropTypes from 'prop-types';
+import AuthContext from '../../../context/auth-context';
 
 class Person extends Component {
   constructor(props) {
@@ -17,6 +18,9 @@ class Person extends Component {
     console.log('[Person.js] rendering...');
     return (
       <React.Fragment>
+        <AuthContext.Consumer>
+          {(context) => context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
+        </AuthContext.Consumer>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
