d5a65cebbbaee57220fd148a26176dc488f41acd
Adam <adamfarhan11@gmail.com>
2019-06-06 11:34:13 -0400

    Completed the burger builder!

--- udemy/burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ udemy/burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,11 +2,15 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
 const toolbar = (props) =>(
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
+        <DrawerToggle click={props.openSideDrawer}>MENU</DrawerToggle>
+        <div className={classes.Logo}>
+            <Logo/>
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavItems />
         </nav>
     </header>
