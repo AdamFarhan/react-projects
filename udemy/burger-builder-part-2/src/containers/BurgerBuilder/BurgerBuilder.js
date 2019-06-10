bf21e5489f3ede06f1d16510296ea24b02d28d0b
Adam Farhan <adam.farhan@di-llc.com>
2019-06-10 06:37:20 -0400

    Burger orders are now sent to the db

--- udemy/burger-builder-part-2/src/containers/BurgerBuilder/BurgerBuilder.js
+++ udemy/burger-builder-part-2/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -21,6 +23,7 @@ class BurgerBuilder extends React.Component{
         totalPrice: 4,
         purchaseable: false,
         purchasing: false,
+        loading: false,
     }
 
     updatePurchaseState = (tempIngredients) => {
@@ -76,13 +79,39 @@ class BurgerBuilder extends React.Component{
         this.setState({purchasing: true});
     }
     purchaseContinueHandler = () => {
-        alert("Purchase Successful!");
+        this.setState({loading: true});
+        //alert("Purchase Successful!");
+        const order ={
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Adam Farhan',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '12345',
+                    country: 'Germany'
+                },
+                email: 'test@test.com'
+            },
+            deliveryType: 'fast'
+        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+                this.setState({loading: false});
+            })
+            .catch(error => {
+                this.setState({loading: false});
+                console.log(error)
+            });
         this.closeModal();
+        
     }
     closeModal = () => {
         this.setState({purchasing: false});
     }
     render(){
+        console.log('[BurgerBuilder.js] render()');
         const disabledInfo = {
             ...this.state.ingredients
         };
@@ -92,11 +121,13 @@ class BurgerBuilder extends React.Component{
         return(
             <React.Fragment>
                 <Modal show={this.state.purchasing} closeModal={this.closeModal}>
+                    {this.state.loading ? <Spinner /> :
                     <OrderSummary
                         ingredients={this.state.ingredients}
                         price={this.state.totalPrice}
                         purchaseCancelled={this.closeModal}
                         purchaseContinued={this.purchaseContinueHandler} />
+                    }
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
