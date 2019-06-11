d392b761f13f6b820b2602a4df7b0faf1705d96c
Adam <adamfarhan11@gmail.com>
2019-06-11 19:17:03 -0400

    Clicking posts now programatically changes page

--- udemy/s10-routing/src/containers/Blog/Posts/Posts.js
+++ udemy/s10-routing/src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 class Posts extends React.Component {
     state = {
         posts: []
@@ -26,16 +26,21 @@ class Posts extends React.Component {
     }
 
     postClickHandler = (id) => {
-        this.setState({selectedPostId: id});
+        this.props.history.push({
+            pathname: '/' + id
+        });
+        //this.props.history.push('/'+id);
     }
     render() {
         const posts = this.state.posts.map(post => {
-            return( <Link to={'/' + post.id} key={post.id}>
+            return( 
+                    // <Link to={'/' + post.id} key={post.id}>
                         <Post 
                         title={post.title} 
                         author={post.author}
                         clicked={() => this.postClickHandler(post.id)}/>
-                    </Link>)
+                    // </Link>
+            )
         });
         return(
             <section className="Posts">
