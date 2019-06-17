f9d887ed16dffb322de882153191e5899821a3ea
Adam Farhan <adam.farhan@di-llc.com>
2019-06-17 11:16:07 -0400

    Added conditional redirct on submit

--- udemy/s10-routing/src/containers/Blog/NewPost/NewPost.js
+++ udemy/s10-routing/src/containers/Blog/NewPost/NewPost.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './NewPost.css';
+import { Redirect } from 'react-router-dom';
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     }
 
     componentDidMount() {
@@ -22,12 +24,17 @@ class NewPost extends Component {
         axios.post('https://jsonplaceholder.typicode.com/posts/', post)
             .then(response => {
                 console.log(response);
+                this.setState({submitted: true});
             });
     }
 
     render () {
         return (
             <div className="NewPost">
+                {/* Rendering the Redirect component will force our
+                    browser to redirect. (It's like running a function) */}
+                {this.state.submitted && 
+                 <Redirect to="/posts" />}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
